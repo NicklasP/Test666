c99dd7787a904229656a681f533e501dd9106e46
NicklasP <yberguru@gmail.com>
2016-09-20 02:07:32 +0200

    Going

---
+++ SmellyCat/src/Packo1/HelloUniverse.java
@@ -0,0 +1,15 @@
+package Packo1;
+
+public class HelloUniverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		HiThere();
+
+	}
+	
+	public static void HiThere(){
+		System.out.println("Planet Tellus22 is now ours, resistance is futile !!!");
+	}
+}
